d8506cc5a87f67c342d5197f8fe2f356c8f1ff9e
emilianov <tipthefuture@gmail.com>
2014-06-12 09:31:15 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fourp::Application.config.secret_key_base = '92056f869b3b9d9107a6b74ed04968ef97b2fdf4c25c8a95a29eae94f22810901d2a884af42752dce309f6a419481b73a37e7ebc0b705133bbe0fbba272e03a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fourp::Application.config.session_store :cookie_store, key: '_fourp_session'
